a1c9da91c313b8760446d971d750b9b44357acaa
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-01-14 12:50:04 -0500

    🔧 build: change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
+import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,6 +10,7 @@ export default defineConfig({
         defineModel: true,
       },
     }),
+    dts(),
   ],
   build: {
     lib: {
