75f35ac87574f5cf600c26a2b4d45012798b6b75
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-01-14 11:27:54 -0500

    ✨ feat: create example

---
+++ examples/with-vite-vue-typescript/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { DatePicker } from "vue-date-picker";
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+let date = new Date();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <DatePicker v-model="date" />
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "vue-datepicker",
+      name: "VueDatePicker",
       fileName: "vue-date-picker",
     },
     rollupOptions: {
