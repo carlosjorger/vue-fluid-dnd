d2b6e2cbba7dd4033f86e37304db5d209b244155
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-01-19 12:08:29 -0500

    ✨ feat: remove not-allowed cursor not using dragging

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,12 @@ var date = new Date();
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <transition-group name="subsection" tag="ul">
     <DragAndDrop
       draggable-id="1"
       enable-drag
       :onDropChange="(_) => {}"
-      tag="li"
+      tag="div"
       style="width: 100px; height: 100px; color: white; background-color: red"
     >
       1
@@ -26,7 +18,7 @@ var date = new Date();
       draggable-id="1"
       enable-drag
       :onDropChange="(_) => {}"
-      tag="li"
+      tag="div"
       style="width: 100px; height: 100px; color: white; background-color: blue"
     >
       2
--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -2,141 +2,84 @@
   <component
     :is="tag"
     :style="style"
-    @dragstart="startDrag($event, draggableId)"
-    @drop="onDrop($event, enableDrag)"
-    @dragenter="onDragEnter($event, draggableId, enableDrag)"
-    @dragover="onDragOver($event)"
-    @dragleave.prevent
-    :draggable="dragging"
-    @dragend="dragEnd($event)"
-    @drag="drag($event)"
     class="draggable"
-    @mousedown="dragging = true"
-    @mouseup="dragging = false"
+    @mousedown="onmousedown($event)"
+    @mousemove="onmousemove($event)"
+    @dragstart="ondragstart()"
+    @mouseup="onmouseup($event)"
   >
     <slot></slot>
   </component>
 </template>
 <script setup lang="ts">
 //TODO: add dragOver functionality https://medium.com/codex/drag-n-drop-with-vanilla-javascript-75f9c396ecd
-import { computed, ref } from "vue";
-const { onDropChange } = defineProps<{
+import { computed, ref, watch } from "vue";
+defineProps<{
   draggableId: string;
-  onDropChange: (id: string) => void;
   enableDrag: boolean;
   tag: string;
   style: {};
 }>();
-
-const markedSection = ref("");
-const IdAttr = `id`;
+const position = ref({ top: 0, left: 0 });
+const offset = ref({ X: 0, Y: 0 });
 const dragging = ref(false);
-const topElement = ref(0);
-const leftElement = ref(0);
-const offsetX = ref(0);
-const offsetY = ref(0);
-const onDragOver = (event: DragEvent) => {
-  if (event.dataTransfer) {
-    event.dataTransfer.dropEffect = "move";
-  }
-};
-const onDragEnter = (
-  event: DragEvent,
-  draggableId: string,
-  enableDrag: boolean
-) => {
-  event.preventDefault();
-  if (!enableDrag) {
-    return;
-  }
-  const eventDataTransfer = event.dataTransfer;
-  if (eventDataTransfer) {
-    eventDataTransfer.dropEffect = "move";
-    eventDataTransfer.effectAllowed = "move";
-  }
-  markedSection.value = draggableId;
-};
-const onDrop = (event: DragEvent, enableDrag: boolean) => {
-  dragging.value = false;
-  const element = event.target as HTMLElement;
-  element.classList.remove("dragging");
-  element.style.top = "";
-  element.style.left = "";
-  element.style.transform = "";
-  element.style.position = "";
-  if (!enableDrag) {
-    return;
-  }
-  const eventDataTransfer = event.dataTransfer;
-  if (eventDataTransfer) {
-    const draggedSectionName = eventDataTransfer.getData(IdAttr);
-    onDropChange(draggedSectionName);
-    markedSection.value = "";
-  }
-};
-const startDrag = (event: DragEvent, draggableId: string) => {
-  dragging.value = true;
-  event.stopPropagation();
-  const eventDataTransfer = event.dataTransfer;
-  if (eventDataTransfer) {
-    eventDataTransfer;
-    var img = new Image();
-    img.src =
-      "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
-    event.dataTransfer.setDragImage(img, 0, 0);
-    eventDataTransfer.setDragImage(img, 0, 0);
-    eventDataTransfer.dropEffect = "move";
-    eventDataTransfer.effectAllowed = "none";
-    eventDataTransfer.setData(IdAttr, draggableId);
-  }
-  const element = event.target as HTMLElement;
-  element.style.opacity = "1";
-  element.classList.add("dragging");
-  const { top, left } = element.getBoundingClientRect();
-  topElement.value = top;
-  leftElement.value = left;
-  offsetX.value = event.offsetX;
-  offsetY.value = event.offsetY;
-};
-const dragEnd = (event: DragEvent) => {
-  console.log("a");
-  dragging.value = false;
-  const element = event.target as HTMLElement;
-  element.classList.remove("dragging");
-  element.style.top = "";
-  element.style.left = "";
-  element.style.transform = "";
-  element.style.position = "";
-};
-const drag = (event: DragEvent) => {
-  event.stopPropagation();
-  dragging.value = false;
-  const element = event.target as HTMLElement;
-  element.style.top = `${topElement.value}px`;
-  element.style.left = `${leftElement.value}px`;
+// const setPosition = (element: HTMLElement, pageX: number, pageY: number) => {
+//   position.value.top = pageX - element.offsetWidth / 2;
+//   position.value.left = pageY - element.offsetHeight / 2;
+//   element.style.left = position.value.top + "px";
+//   element.style.top = position.value.left + "px";
+// };
+const setTransform = (element: HTMLElement, pageX: number, pageY: number) => {
+  element.style.top = `${position.value.top}px`;
+  element.style.left = `${position.value.left}px`;
   const { height } = element.getBoundingClientRect();
   element.style.transform = `translate( ${
-    event.x - leftElement.value - offsetX.value
-  }px, ${event.y - topElement.value - offsetY.value - height / 2}px)`;
+    pageX - position.value.left - offset.value.X
+  }px, ${pageY - position.value.top - offset.value.Y - height / 2}px)`;
+};
+const onmousemove = function (event: DragEvent) {
+  if (!dragging.value) {
+    return;
+  }
+  const element = event.target as HTMLElement;
+  setTransform(element, event.x, event.y);
+};
+const onmousedown = function (event: DragEvent) {
+  const element = event.target as HTMLElement;
+  dragging.value = true;
+  element.style.position = "absolute";
+  element.style.zIndex = "1000";
+  const { top, left } = element.getBoundingClientRect();
+  position.value.top = top;
+  position.value.left = left;
+  offset.value.X = event.offsetX;
+  offset.value.Y = event.offsetY;
+  element.style.transition = "";
+};
+const onmouseup = (event: DragEvent) => {
+  console.log("onmouseup");
+  dragging.value = false;
+  const element = event.target as HTMLElement;
+  element.style.position = "";
+  element.style.top = "";
+  element.style.left = "";
+  element.style.transform = "";
+  element.style.transition = "transform 0.3s ease";
+};
+const ondragstart = () => {
   return false;
 };
 const computedCursor = computed(() => (dragging.value ? "grabbing" : "grab"));
+watch(computedCursor, (value) => {
+  console.log(value);
+});
 </script>
 <style>
 .draggable {
   position: initial;
-  /* transition: all 0.5s ease; */
 }
 .draggable:hover {
-  cursor: v-bind("computedCursor");
-}
-.draggable:active {
-  background-color: aqua;
-}
-.dragging {
-  cursor: grabbing !important;
-  position: fixed !important;
+  cursor: v-bind("computedCursor") !important;
 }
 </style>
-<!-- TODO: remove not-allowed cursor -->
 <!-- TODO:dragEnd is called whe the element is dragged in the top part -->
