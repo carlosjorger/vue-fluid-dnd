ddc3217c2ffa5fd15be52d879320ecf0892516cc
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-02-06 11:33:08 -0500

    ✨ feat: create SingleVerticalList example

---
+++ docs/src/components/SingleVerticalList.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>A</h1>
+  <div>
+    <div
+      class="ball"
+      v-for="(element, index) in list"
+      :style="{ 'background-color': element.backgroundColor }"
+    ></div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { Draggable, Droppable } from "vue3-juice-dnd";
+const list = ref([
+  {
+    "draggable-id": "d1",
+    backgroundColor: "red",
+  },
+  {
+    "draggable-id": "d2",
+    backgroundColor: "yellow",
+  },
+  {
+    "draggable-id": "d3",
+    backgroundColor: "blue",
+  },
+]);
+</script>
+<style>
+.ball {
+  border-color: white;
+  margin-top: 10px;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+}
+</style>
