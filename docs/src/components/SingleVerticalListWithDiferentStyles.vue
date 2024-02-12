8ae70089a45a0e0dffe397f1241fc209d9145bfa
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-02-11 19:30:35 -0500

    ✨ feat: add more styles to Single Vertical list with diferent styles

--- docs/src/components/SingleVerticalListWithDiferentStyles.vue
+++ docs/src/components/SingleVerticalListWithDiferentStyles.vue
@@ -5,19 +5,19 @@ const list = ref([
   {
     id: "1",
     style:
-      "margin: 30px 0px !important; border-style: solid; border-width: 0.8rem;",
+      "margin: 30px 0px !important; border-style: solid; border-width: 0.8rem; padding: 5px;",
     content: "1",
   },
   {
     id: "2",
     style:
-      "margin: 20px 0px !important; border-style: solid; border-width: 0.6rem;",
+      "margin: 20px 0px !important; border-style: solid; border-width: 0.6rem; padding: 10px;",
     content: "2",
   },
   {
     id: "3",
     style:
-      "margin: 10px 0px !important; border-style: solid; border-width: 0.4rem;",
+      "margin: 10px 0px !important; border-style: solid; border-width: 0.4rem; padding: 15px;",
     content: "3",
   },
 ]);
