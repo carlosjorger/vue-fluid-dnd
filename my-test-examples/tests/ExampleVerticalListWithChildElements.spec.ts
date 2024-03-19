9d53b3e8a10788739902afe758546a6e9bda6ea4
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-03-18 20:57:28 -0400

    🧪 test: keep working on dragDrop test

--- my-test-examples/tests/ExampleVerticalListWithChildElements.spec.ts
+++ my-test-examples/tests/ExampleVerticalListWithChildElements.spec.ts
@@ -19,7 +19,30 @@ test("has title", async ({ page }) => {
 });
 //TODO Add drag and drop from top and from bottom
 test("drag and drop top-down", async ({ page }) => {
-  await page
-    .locator("#child-with-children-1")
-    .dragTo(page.locator("#child-with-children-6"));
+  await page.goto("/");
+
+  await dragDrop(page, "#child-with-children-1", "#child-with-children-6");
+  await expect(page.locator("ul > li")).toHaveText([
+    "6 6789",
+    "2 2345",
+    "3 3456",
+    "4 4567",
+    "5 5678",
+    "1 1234",
+  ]);
 });
+async function dragDrop(
+  page: Page,
+  originSelector: string,
+  destinationSelector: string
+) {
+  const originElement = await page.waitForSelector(originSelector);
+  const destinationElement = await page.waitForSelector(destinationSelector);
+
+  await originElement.hover();
+  await page.mouse.down();
+  const box = (await destinationElement.boundingBox())!;
+  await page.mouse.move(box.x, box.y + box.height / 2, { steps: 5 });
+  await destinationElement.hover();
+  await page.mouse.up();
+}
