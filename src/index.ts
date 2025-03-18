a96973f24b96f86c3e341ae2cb7f4deff6fba617
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2025-03-18 11:18:28 -0400

    ✨ feat: export important types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 import useDragAndDrop from "./composables/useDragAndDrop";
+import { DragEndEventData, DragStartEventData, Direction } from "./composables";
 export { useDragAndDrop };
+export type {DragStartEventData, DragEndEventData, Direction}
